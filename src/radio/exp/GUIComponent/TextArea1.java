852ab7750c1e3eb973b257f0c9dfed5b79c721ba
wuzhiyang <18062966456@163.com>
2019-05-04 16:35:26 +0800

    新增功能【GUI】新建JTextArea范例，可滚动

---
+++ src/radio/exp/GUIComponent/TextArea1.java
@@ -0,0 +1,41 @@
+package radio.exp.GUIComponent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author wzy
+ */
+public class TextArea1 implements ActionListener {
+    JTextArea text;
+
+    public void go() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Just do it!");
+        button.addActionListener(this);
+        text = new JTextArea(10, 20);
+        text.setLineWrap(true);
+
+        JScrollPane scrollPane = new JScrollPane(text);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        panel.add(scrollPane);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.setSize(350, 300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        text.append("button clicked!\n");
+    }
+
+    public static void main(String[] args) {
+        TextArea1 textArea1 = new TextArea1();
+        textArea1.go();
+    }
+}
